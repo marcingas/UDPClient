02f1570e64f6e29767d13c3e072ba8c94e6c0467
Marcin <gasiorek.mail@gmail.com>
2023-04-03 10:51:02 +0200

    set up client socket

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try{
+            InetAddress adress = InetAddress.getLocalHost();
+            DatagramSocket datagramSocket = new DatagramSocket();
+            Scanner scanner = new Scanner(System.in);
+            String echoString;
+            do{
+                System.out.println("Enter string to be echoed: ");
+                echoString= scanner.nextLine();
+                byte[] buffer = echoString.getBytes();
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, adress,5000);
+                datagramSocket.send(packet);
+            }while (!echoString.equals("exit"));
+
+
+        }catch (SocketTimeoutException e){
+            System.out.println("Socket timed out");
+        }catch (IOException e){
+            System.out.println("Client error: " + e.getMessage());
+        }
     }
 }
